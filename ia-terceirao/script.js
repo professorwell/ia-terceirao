684f0cf52162689d681a2a34dee721b8315edcc8
professorwell <141196100+professorwell@users.noreply.github.com>
2024-08-01 10:31:51 -0300

    Add files via upload

--- ia-terceirao/script.js
+++ ia-terceirao/script.js
@@ -9,24 +9,67 @@ const perguntas = [
         enunciado: "os animais são inofensivos e a maioria precisa de ajuda. como ajudá los?",
 
         alternativas: [
-            "voce pode estar adotando um animalzinho.", 
-            "abrigo de animais.."
+            {
+                texto: "voce pode estar adotando um animalzinho.",
+                afirmacao: "afirmacao"
+
+            },
+            {
+                texto: "abrigo de animais.",
+                afirmacao: "afirmacao"
+            }
+
         ]
     },
     {
         enunciado: "Plantar árvores frutíferas é essencial para sustentar o meio ambiente e fornecer alimentos frescos aumentando a quantidade de árvores e melhorando a qualidade de vida da população. Qual seria a melhor maneira para começar essa iniciativa?",
         alternativas: [
-            "Escolha espécies frutíferas adequadas á sua região, prepare bem o solo e plante em áreas com boa luz solar.",
- 
-            "Escolher lugares estratégicos onde podemos aproveitar ao máximo os benefícios que as árvores tem a oferecer. "
+            {
+                texto: "Escolha espécies frutíferas adequadas á sua região, prepare bem o solo e plante em áreas com boa luz solar.",
+                afirmacao: "afirmaccao"
+            },
+            {
+                texto: "Escolher lugares estratégicos onde podemos aproveitar ao máximo os benefícios que as árvores tem a oferecer. ",
+                afirmacao: "afirmacao"
+            }
         ]
     },
     {
         enunciado: "Museus são importantes para ver a trajetória de nossos antepassados,  mas um dia nós seremos a história. O que você quer deixar para a história?",
 
         alternativas: [
-            "MEu quero deixar um legado.", 
-            "Eu não ligo para história o que importa é o agora."
+            {
+                texto: "eu quero deixar um legado!",
+                afirmacao: " afirmacao"
+            },
+            {
+                texto: "eu não ligo para a historia, oque importa é o agora, e o futuro!",
+                afirmacao: "afirmacao"
+            }
+
         ]
-    }  
+    },
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++;
+            mostrarPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
