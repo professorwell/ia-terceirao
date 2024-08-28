ea03f5c950a35a97078ce8ede07a06b374b93268
professorwell <141196100+professorwell@users.noreply.github.com>
2024-08-28 18:25:11 -0300

    Add files via upload

--- ia-terceirao/script.js
+++ ia-terceirao/script.js
@@ -11,12 +11,12 @@ const perguntas = [
         alternativas: [
             {
                 texto: "voce pode estar adotando um animalzinho.",
-                afirmacao: "afirmacao"
+                afirmacao: "você é uma pessoa muito boa, que preza ao cuidados dos animais"
 
             },
             {
                 texto: "abrigo de animais.",
-                afirmacao: "afirmacao"
+                afirmacao: "você é uma pessoa que preza a resolver os problemas com a razão"
             }
 
         ]
@@ -26,11 +26,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Escolha espécies frutíferas adequadas á sua região, prepare bem o solo e plante em áreas com boa luz solar.",
-                afirmacao: "afirmaccao"
+                afirmacao: "alem da boa intenção de reflorestar, seria uma otima opção para um piquinique"
             },
             {
                 texto: "Escolher lugares estratégicos onde podemos aproveitar ao máximo os benefícios que as árvores tem a oferecer. ",
-                afirmacao: "afirmacao"
+                afirmacao: "exelente ideia"
             }
         ]
     },
@@ -40,11 +40,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "eu quero deixar um legado!",
-                afirmacao: " afirmacao"
+                afirmacao: "você é uma pessoa decidida"
             },
             {
                 texto: "eu não ligo para a historia, oque importa é o agora, e o futuro!",
-                afirmacao: "afirmacao"
+                afirmacao: "você não se preocupa muito com as opnião de terceiros"
             }
 
         ]
@@ -53,23 +53,39 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta() {
+function mostraPergunta(){
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
-        botaoAlternativas.addEventListener("click", function(){
-            atual++;
-            mostrarPergunta();
-        })
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+
 }
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++
+    mostraPergunta();
+}
+function mostraResultado(){
+    caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+
 
 mostraPergunta();
